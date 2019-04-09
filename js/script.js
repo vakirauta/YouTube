18bab342a4121587d22b2aed0581dead52615862
vakirauta <vakirauta1989@gmail.com>
2019-04-09 22:03:30 +0300

    Второй день интенсива

--- js/script.js
+++ js/script.js
@@ -99,5 +99,80 @@ more.addEventListener('click', () => {
         setTimeout(() => {
             card.classList.remove('videos__item-active');
         }, 10);
+        bindNewModal(card);
+    }
+
+    sliceTitle('.videos__item-descr', 100); 
+});
+
+function sliceTitle(selector, count) {
+    document.querySelectorAll(selector).forEach(item => {
+        item.textContent.trim();
+
+        if (item.textContent.length < count) {
+            return;
+        } else {
+            const str = item.textContent.slice(0, count + 1) + "...";
+            item.textContent = str;
+        }
+    });
+}
+sliceTitle('.videos__item-descr', 80);
+
+function openModal(){
+  modal.style.display = 'block';
+}
+
+function closeModal(){
+    modal.style.display = 'none';
+    player.stopVideo();
+  }
+
+function bindModal(cards) {
+    cards.forEach(item => {
+      item.addEventListener('click', (e) => {
+        e.preventDefault();
+        const id = item.getAttribute('data-url');
+        loadVideo(id);
+        openModal();
+      });
+    });
+  }
+bindModal(videos);
+
+function bindNewModal(cards) {
+    cards.addEventListener('click', (e) => {  //Откр-ет модальное окно одной карточки
+      e.preventDefault();
+      const id = cards.getAttribute('data-url');
+        loadVideo(id);
+      openModal();
+    });
+  }
+
+modal.addEventListener('click', (e) => {
+    if(!e.target.classList.contains('modal__body')) {
+        closeModal();
     }
 });
+
+function  createVideo() {
+    var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      setTimeout(() => {                      //Делаем задержку
+        player = new YT.Player('frame', {    //Создаём новый экземпляр плеера
+          height: '100%',                      //Настройки
+          width: '100%',
+          videoId: 'M7lc1UVf-VE'
+        });
+      }, 300);
+    }
+
+createVideo();
+
+function loadVideo(id) {    //Загружаем новое видео по id
+    player.loadVideoById({'videoId': `${id}`});
+  }
